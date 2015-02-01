5b6582c9fd6f9ccf36934ae867b585ac669d2fec
Fernando Niwes <fniwes@gmail.com>
2015-02-01 19:16:38 -0300

    Presentation Controller using grails3 video as test. Hook to check when video time changes. List of slides and time of each slide.

---
+++ grails-app/controllers/infoqcaster/PresentationController.groovy
@@ -0,0 +1,9 @@
+package infoqcaster
+class PresentationController {
+
+    def index() {
+        // Grails 3 requires a controller to return a map to render the view.
+        // otherwise it just return a empty response
+        return [:]
+    }
+}
