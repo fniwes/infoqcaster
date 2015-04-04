aae719de70fb6207e090007dfdbf1a06ab09620b
Fernando Niwes <fernando@soluciones3f.com.ar>
2015-04-04 02:57:57 -0300

    Removed Presentation prefix from all presentations name

--- grails-app/controllers/infoqcaster/SelectorController.groovy
+++ grails-app/controllers/infoqcaster/SelectorController.groovy
@@ -4,7 +4,12 @@ class SelectorController {
         // Get Presentations from RSS Feed
         def xml = new XmlSlurper().parse("http://www.infoq.com/feed/presentations?token=NpiZ08bVi2xgRxi2BSXi0ArBW7CI67gi")
         return [items: xml.channel.item.collect {
-            [title: it.title, url: it.link, description: it.description]
+            [title: adaptPresentationName(it.title.toString()), url: it.link, description: it.description]
         }]
+    }
+
+    private String adaptPresentationName(String name) {
+        if (name.startsWith("Presentation: ")) return name[14..-1]
+        return name
     }
 }
